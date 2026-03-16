468dc9fe52b6482d6fc06173141774c816762f59
gustavozequim <gustavozequim9@gmail.com>
2026-03-16 15:08:38 -0300

    Altera .gitignore e adiciona .env

--- lqm_converter.py
+++ lqm_converter.py
@@ -3,23 +3,26 @@ import cv2
 import pytesseract
 from pathlib import Path
 from zipfile import ZipFile
+from dotenv import load_dotenv
+load_dotenv()
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\158666389\Desktop\tseract\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = os.getenv('CAMINHO_TSERACT')
 
 def converte_lqm():
     contagem = 0
     pasta = Path('./')
+    print(pasta)
     caminho_imagem = Path('./images')
     caminho_txt = Path('./txt')
     caminho_txt.mkdir(exist_ok=True)
 
     for raiz, pastas, arquivos in os.walk(pasta):
         print(f"Diretório atual: {raiz}")
-    for arquivo in arquivos:
-        print(f" - Arquivo: {os.path.join(raiz, arquivo)}")
-        if arquivo.endswith('.zip'):
-            with ZipFile(arquivo, 'r') as zip_out:
-                zip_out.extractall()
+        for arquivo in arquivos:
+            print(f" - Arquivo: {os.path.join(raiz, arquivo)}")
+            if arquivo.endswith('.zip'):
+                with ZipFile(arquivo, 'r') as zip_out:
+                    zip_out.extractall()
               
     if caminho_imagem.exists():
         contagem += 1

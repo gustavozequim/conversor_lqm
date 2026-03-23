03c04e38193417278e8d54413c88d4529f467c9d
gustavozequim <gustavozequim9@gmail.com>
2026-03-23 14:23:06 -0300

    Adiciona 'nomeclaturas' para cada secao. Melhora execucao. Adiciona caminho das notas no .env_model

--- detecta_arquivo.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def detecta_arquivo(pasta, extensao):
-    for raiz, pastas, arquivos in os.walk(pasta):
-            print(f"Diretório atual: {raiz}")
-    for arquivo in arquivos:
-        print(f" - Arquivo: {os.path.join(raiz, arquivo)}")
